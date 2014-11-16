b3b793a4c49847f415ae2546e8c4fd216dee940b
Jose Navarrete <janavarretecristino@gmail.com>
2014-11-16 13:34:39 +0100

    #10 moved to angular-ui-router and added # back to url.

--- baseApp/static/js/app.js
+++ baseApp/static/js/app.js
@@ -1,23 +1,36 @@
 /**
  * Created by josenava on 05/11/14.
  */
-var app = angular.module('movementApp', ['ngRoute']);
+var app = angular.module('movementApp', ['ui.router']);
 
-app.config(['$httpProvider', '$routeProvider', '$locationProvider', function($httpProvider, $routeProvider, $locationProvider) {
+app.config(function($httpProvider, $stateProvider, $urlRouterProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-    $routeProvider.
-        when('/addMovements', {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('addMovements', {
+            url: '/addMovements',
             controller: 'UploadMovementsCtrl',
             templateUrl: '/static/templates_angularViews/uploadMovements.html'
         })
-        .when('/movements', {
+        .state('movements', {
+            url: '/movements',
             controller: 'ShowCtrl',
             templateUrl: '/static/templates_angularViews/movements.html'
-        })
-        .otherwise({
-            redirectTo: '/'
         });
-    $locationProvider.html5Mode(true);
-}]);
+//    $routeProvider.
+//        when('/addMovements', {
+//            controller: 'UploadMovementsCtrl',
+//            templateUrl: '/static/templates_angularViews/uploadMovements.html'
+//        })
+//        .when('/movements', {
+//            controller: 'ShowCtrl',
+//            templateUrl: '/static/templates_angularViews/movements.html'
+//        })
+//        .otherwise({
+//            redirectTo: '/'
+//        });
+//    $locationProvider.html5Mode(true);
+});
